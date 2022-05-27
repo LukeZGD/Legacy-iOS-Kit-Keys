6ffb1f5c13d03f7e4583d3eed1ad14909f21ce74
LukeZGD <l32luke@gmail.com>
2022-05-27 20:21:00 +0800

    Update

--- firmware.sh
+++ firmware.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-devices=(iPhone4,1 iPhone5,1 iPhone5,2 iPhone5,3 iPhone5,4 iPad2,1 iPad2,2 iPad2,3 iPad2,4 iPad2,5 iPad2,6 iPad2,7 iPad3,1 iPad3,2 iPad3,3 iPad3,4 iPad3,5 iPad3,6 iPod5,1)
+devices=(iPhone3,1 iPhone3,2 iPhone3,3 iPhone4,1 iPhone5,1 iPhone5,2 iPhone5,3 iPhone5,4 iPad2,1 iPad2,2 iPad2,3 iPad2,4 iPad2,5 iPad2,6 iPad2,7 iPad3,1 iPad3,2 iPad3,3 iPad3,4 iPad3,5 iPad3,6 iPod5,1)
 
 for device in ${devices[@]}; do
     echo $device
@@ -21,6 +21,8 @@ for device in ${devices[@]}; do
         mkdir $build
         cd $build
         curl -L "http://127.0.0.1:8888/firmware/$device/$build" -o index.html
+        curl -LO https://api.ipsw.me/v2.1/$device/$build/sha1sum
+        curl -LO https://api.ipsw.me/v2.1/$device/$build/url
         cd ..
         [[ ! -s $build/index.html ]] && rm -rf $build
     done
