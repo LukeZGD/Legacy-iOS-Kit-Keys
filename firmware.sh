2c78f1f7bacd6d54e3270624b01ed9e35992a45d
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2025-02-25 20:36:08 +0800

    an update

--- firmware.sh
+++ firmware.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#devices=(iPhone2,1 iPhone3,1 iPhone3,2 iPhone3,3 iPhone4,1 iPhone5,1 iPhone5,2 iPhone5,3 iPhone5,4 iPad1,1 iPad2,1 iPad2,2 iPad2,3 iPad2,4 iPad2,5 iPad2,6 iPad2,7 iPad3,1 iPad3,2 iPad3,3 iPad3,4 iPad3,5 iPad3,6 iPod2,1 iPod3,1 iPod4,1 iPod5,1)
+devices=(iPhone1,1 iPhone1,2 iPhone2,1 iPhone3,1 iPhone3,2 iPhone3,3 iPhone4,1 iPhone5,1 iPhone5,2 iPhone5,3 iPhone5,4
+         iPad1,1 iPad2,1 iPad2,2 iPad2,3 iPad2,4 iPad2,5 iPad2,6 iPad2,7 iPad3,1 iPad3,2 iPad3,3 iPad3,4 iPad3,5 iPad3,6
+         iPod1,1 iPod2,1 iPod3,1 iPod4,1 iPod5,1)
 #devices=(iPhone8,1 iPhone8,2 iPhone8,4 iPhone9,1 iPhone9,2 iPhone9,3 iPhone9,4 iPad5,1 iPad5,2 iPad5,3 iPad5,4 iPod9,1)
-devices=(iPhone7,1 iPhone7,2 iPod7,1)
+#devices=(iPhone7,1 iPhone7,2 iPod7,1)
 
 for device in ${devices[@]}; do
     echo $device
@@ -33,7 +35,7 @@ for device in ${devices[@]}; do
         echo $build
         mkdir $build
         pushd $build
-        curl -L "http://127.0.0.1:8888/firmware/$device/$build" -o index.html
+        curl -L "https://api.m1sta.xyz/wikiproxy/$device/$build" -o index.html
         #curl -LO https://api.ipsw.me/v2.1/$device/$build/sha1sum
         #curl -LO https://api.ipsw.me/v2.1/$device/$build/url
         popd # $build
